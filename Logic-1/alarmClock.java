794f07d7370041518ebbfeb5649951c0ddc6c6e5
Hadil <220842344+had33lz@users.noreply.github.com>
2025-07-28 13:23:50 +0400

    Add solution for Logic-1-alarmClock

---
+++ Logic-1/alarmClock.java
@@ -0,0 +1,9 @@
+public String alarmClock(int day, boolean vacation) {
+    boolean isWeekend = (day == 0 || day == 6);
+    
+    if (vacation) {
+        return isWeekend ? "off" : "10:00";
+    } else {
+        return isWeekend ? "10:00" : "7:00";
+    }
+}
